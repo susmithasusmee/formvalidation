ea8396c3945ba0be7ff3d114c39d187336d2bc3c
Susmitha <susmithakurapati321@gmail.com>
2024-01-25 16:21:27 +0530

    first commit

---
+++ src/FromValid.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const FromValid = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label className="input-group-text label">{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FromValid;
